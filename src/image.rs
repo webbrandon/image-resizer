943f3efce3b68beedef3571d88734650ead7e435
Brandon Clark <brandon.clark@leafgroup.com>
2018-09-06 16:49:30 -0700

    Adding new functionality test.

--- src/image.rs
+++ src/image.rs
@@ -7,18 +7,50 @@ use std::time::SystemTime;
 fn main() -> Result<(), Box<Error>> {
     // Args arrangement
     let mut args = env::args().skip(1);
-    assert_eq!(args.len(), 3, "Arguments must be: file_location width height");
-
+    //assert_eq!(args.len(), 3, "Arguments must be: file_location width height");
+    
     // Reading args
     let file_location = args.next().unwrap();
     let width = args.next().unwrap().parse()?;
     let height = args.next().unwrap().parse()?;
-
+    let thumbnail:bool = if args.len() == 1 {
+        false
+    } else {
+        true
+    };
     // Do the job
     let now = SystemTime::now();
     let img = image::open(&file_location)?;
-    // let mini = img.resize(width, height, image::imageops::CatmullRom);
-    let resized = img.thumbnail_exact(width, height);
+    let mut resized:image::DynamicImage = image::DynamicImage::new_rgba8(0, 0);
+    if thumbnail {
+        println!("Resizing as thumbnail");
+        resized = img.thumbnail_exact(width, height);
+    } else {
+        let sample_type = args.next().unwrap();
+        match sample_type.as_str() {
+            "l" => {
+                println!("Resizing with Lanczos3");
+                resized = img.resize(width, height, image::imageops::Lanczos3)
+            },
+            "g" => {
+                println!("Resizing with Gaussian");
+                resized = img.resize(width, height, image::imageops::Gaussian)
+            },
+            "c" => {
+                println!("Resizing with CatmullRom");
+                resized = img.resize(width, height, image::imageops::CatmullRom)
+            },
+            "n" => {
+                println!("Resizing with Nearest");
+                resized = img.resize(width, height, image::imageops::Nearest)
+            },
+            "t" => {
+                println!("Resizing with Triangle");
+                resized = img.resize(width, height, image::imageops::Triangle)
+            },
+            _ => ()
+        }
+    }
     if let Ok(elapsed) = now.elapsed() {
         println!(
             "resizing: {}.{:03}",
